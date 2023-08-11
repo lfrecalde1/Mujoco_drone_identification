f24f28a1e7bc693b959b04689b99499e47532388
fer <fer@pop-os.localdomain>
2023-08-11 12:57:25 -0300

    Yaw

--- Velocities_data.m
+++ Velocities_data.m
@@ -247,8 +247,8 @@ switch n
         %%
     case 12
         close all
-        ul_ref=0.2*sin(0.75*t).*cos(0.8*t)-0.15*cos(t);
-        um_ref=0.4*cos(0.1*t)+0.3*cos(0.4*t);
+        ul_ref=2.5*cos(t).*sin(0.5*t)+0.3*cos(0.7*t).*cos(0.3*t);
+        um_ref=2.5*sin(t).*sin(0.5*t)+0.3*cos(0.7*t).*cos(0.3*t);
         un_ref = 2.5*sin(t).*sin(0.5*t)+0.3*cos(0.7*t).*cos(0.3*t);
         w_ref = 2.5*sin(t).*sin(0.5*t)+0.3*cos(0.7*t).*cos(0.3*t);
         subplot(4,1,1)
